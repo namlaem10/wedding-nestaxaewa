cb1b23a3e3694b655dfa38ba4d30cafbaca004d8
namlaem10 <hoangnam08101998@gmail.com>
2024-12-30 10:47:49 +0700

    Remove log

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-console.log('Logging .env variables')
-console.log(process.env.NEXT_PUBLIC_SUPABASE_URL)
-console.log(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
