a2607c9bbaa65fbe686724057adc1db75eac4f88
namtran198 <nam.tran@realstake.io>
2024-12-30 10:31:04 +0700

    Add log

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log('Logging .env variables')
+console.log(process.env.NEXT_PUBLIC_SUPABASE_URL)
+console.log(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
